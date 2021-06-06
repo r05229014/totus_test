ba20564a1d632d5129b2ec9ce56188750acf11ef
r05229014 <r05229014@ntu.edu.tw>
2021-06-06 23:36:21 +0800

    'remove_no_need'

--- get_link.py
+++ get_link.py
@@ -5,7 +5,6 @@ from numpy import dot
 from numpy.linalg import norm
 
 
-
 def cos_sim(a,b):
     return dot(a, b)/(norm(a)*norm(b))
 
@@ -20,17 +19,8 @@ def get_corps(json_path):
     return corpus
 
 
-# def clean_data(texts):
-#     '''處理stop words'''
-#     cleaned_corpus = []
-#     for c in texts:
-#         cleaned_corpus.append(remove_stopwords(c))
-#     return cleaned_corpus
-
-
 def get_TFIDF(texts, vectorizer):
     '''TFIDF training'''
-    # vectorizer = TfidfVectorizer()
     tfidf = vectorizer.fit_transform(texts)
     return tfidf.toarray()
 
@@ -43,16 +33,6 @@ def get_scores(tfidf):
     scores = np.where(scores > 0.999, 0, scores)
     return scores
 
-
-
-# def get_scores(tfidf):
-#     '''得到每個文本相對應的相似度'''
-#     tfidfs = []
-#     for i in range(len(corpus)):
-#         tfidfs.append(cosine(tfidf[i:i+1], tfidf))
-#     tfidfs = np.stack(tfidfs).squeeze()
-#     tfidfs = np.where(tfidfs>0.99, 0, tfidfs).flatten()
-#     return tfidfs
 
 def get_links(corpus_count, scores, max_count=3):
     scores = scores.flatten()
@@ -72,14 +52,6 @@ def get_links(corpus_count, scores, max_count=3):
     for i in range(max_count):
         links.append([df[2*i, 1].astype(int), df[2*i, 2].astype(int)])
     return links
-# def get_links(corpus_count, scores, max_count=3):
-#     '''得到前max_count的連結, 1 ---> 2 表示文章是從第1篇連到第2篇'''
-#     indexs = np.argsort(scores.flatten())[-1:-max_count-1:-2]
-#     links = []
-#     for i in indexs:
-#         s, d = i//corpus_count +1, i%corpus_count
-#         links.append([s,d])
-#     return links
 
 
 if __name__ == '__main__':
